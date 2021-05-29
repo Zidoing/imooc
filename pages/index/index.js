c090a33ca7ffa4faad2ce4162c38da4cb235261e
zhoulei <403907871@qq.com>
2021-05-29 21:05:32 +0800

    - 数据循环及代码量

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,22 +4,57 @@ const app = getApp()
 
 Page({
     data: {
-        name: "hello sss",
-        showIcon: true
+        showIcon: true,
+        swiperList: [],
+        courses: [],
+        searchList: null,
+        type: "recommend",
+        tabs: [
+            {name: "推荐", type: "recommend"},
+            {name: "路径", type: "path"},
+            {name: "实战", type: "project"},
+            {name: "活动", type: "activity"},
+        ]
     },
 
     handleInputChange(e) {
         console.log(e)
         const value = e.detail.value;
+        let searchList = null;
+        if (value) {
+            searchList = this.data.courses.filter(item => item.title.indexOf(value) > -1)
+        }
+
         if (value) {
             this.setData({
-                showIcon: false
+                showIcon: false,
+                searchList,
             });
         } else {
             this.setData({
-                showIcon: true
-            })
+                showIcon: true,
+                searchList
+            });
         }
+    },
+    onLoad() {
+        wx.request({
+                url: "https://www.fastmock.site/mock/7b84fef3e3467bc1457dc7fff158c5a7/weixin/api/getData",
+                success: (res) => {
+                    const {data: {data}} = res
+                    const {swiperList, courses} = data
+                    this.setData({
+                        swiperList,
+                        courses
+                    })
+                }
+            }
+        )
+    },
+    changeType(e) {
+        const type = e.currentTarget.dataset.type;
+        this.setData({type});
     }
+
 
 })
